e21a01f1c0f5be69366805f6babc951ed6b4e2d3
Luan Carlos <luan.carlos@safetysystemtechnology.com.br>
2020-04-07 18:19:11 -0300

    finished the first challenge

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,97 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.status(200).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const uid = uuid();
+
+  const repository = { uid, title, url, techs, likes: 0 };
+
+  repositories.push(repository);
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  if (!id) {
+    return response.status(400).json({
+      message: "The id is necessary to edit a repository",
+    });
+  }
+
+  const checkRepository = repositories.find(
+    (repository) => repository.uid == id
+  );
+
+  if (!checkRepository) {
+    return response.status(400).json({
+      message: "Repository not find",
+    });
+  }
+
+  const updatevalues = (value) => {
+    if (value === "title") {
+      checkRepository.title = title;
+    }
+    if (value === "url") {
+      checkRepository.url = url;
+    }
+    if (value === "techs") {
+      checkRepository.techs = techs;
+    }
+  };
+
+  Object.keys(request.body).map((item) => item && updatevalues(item));
+
+  response.json({ checkRepository });
 });
 
 app.delete("/repositories/:id", (req, res) => {
-  // TODO
+  const { id } = req.params;
+
+  const checkRepository = repositories.find(
+    (repository) => repository.uid == id
+  );
+
+  if (!checkRepository) {
+    return res.status(400).json({
+      message: "Repository not find",
+    });
+  }
+
+  const repositoryId = repositories.indexOf(checkRepository);
+
+  if (repositoryId !== -1) {
+    repositories.splice(repositoryId, 1);
+    return res.status(204).json();
+  }
+
+  return res.status(400).json({
+    message: "Repository not find",
+  });
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const checkRepository = repositories.find(
+    (repository) => repository.uid == id
+  );
+
+  if (!checkRepository) {
+    return response.status(400).json({
+      message: "Repository not find",
+    });
+  }
+
+  checkRepository.likes = checkRepository.likes + 1;
+
+  return response.status(201).json(checkRepository.likes);
 });
 
 module.exports = app;
